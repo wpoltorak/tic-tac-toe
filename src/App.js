4dd070e6217c17b52bcc838ec28ee37382d61650
Wojtek Poltorak <wojciech.poltorak@eclipsegroup.co.uk>
2024-02-27 17:26:55 +0100

    game event handling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 /*function App() {
   return (
@@ -24,7 +25,73 @@ import './App.css';
 }
 */
 
-function Square() {
-  return <button className="square">X</button>;
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+
+  function handleClick(index) {
+    if (squares[index] || calculateWinner(squares)) {
+      return;
+    }
+    const newSquares = squares.slice();
+    newSquares[index] = xIsNext ? 'X' : 'O';
+    setXIsNext(!xIsNext);
+    setSquares(newSquares)
+
+  }
+
+  function calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+
+      if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
 }
-export default Square;
+
+function Square({ value, onSquareClick }) {
+
+
+
+  return (
+    <>
+      <button className="square" onClick={onSquareClick}>{value}</button>
+    </>
+  );
+}
+export default Board;
